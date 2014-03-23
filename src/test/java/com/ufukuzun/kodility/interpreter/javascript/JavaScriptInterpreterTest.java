d007f1966c39d394de2c7cb8021c8125cf944970
ufukuzun <ufuk.uzun@prime.com.tr>
2014-03-23 19:36:28 +0200

    basic javascript interpreting implemented

--- src/test/java/com/ufukuzun/kodility/interpreter/javascript/JavaScriptInterpreterTest.java
+++ src/test/java/com/ufukuzun/kodility/interpreter/javascript/JavaScriptInterpreterTest.java
@@ -20,4 +20,16 @@ public class JavaScriptInterpreterTest {
         assertThat(Double.parseDouble(result.toString()), equalTo(2d));
     }
 
+    @Test
+    public void shouldReturnNoResultMessageIfEvaluationResultIsNull() {
+        Object result = interpreter.interpret("function foo() {} foo();");
+        assertThat(result.toString(), equalTo("No Result"));
+    }
+
+    @Test
+    public void shouldReturnExceptionMessageIfEvaluationCauseAnException() {
+        Object result = interpreter.interpret("foo();");
+        assertThat(result.toString(), equalTo("sun.org.mozilla.javascript.internal.EcmaError: ReferenceError: \"foo\" is not defined. (<Unknown source>#1) in <Unknown source> at line number 1"));
+    }
+
 }
