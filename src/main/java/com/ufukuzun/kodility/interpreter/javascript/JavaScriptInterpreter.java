650176458b609cf601288ffb210bf2720905829f
Ufuk Uzun <ufukuzun.ce@gmail.com>
2014-03-23 20:00:09 +0200

    javascript error message is refined (but line number is still wrong)

--- src/main/java/com/ufukuzun/kodility/interpreter/javascript/JavaScriptInterpreter.java
+++ src/main/java/com/ufukuzun/kodility/interpreter/javascript/JavaScriptInterpreter.java
@@ -12,13 +12,21 @@ public class JavaScriptInterpreter {
     public Object interpret(String script) {
         ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
         ScriptEngine javaScriptEngine = scriptEngineManager.getEngineByName("JavaScript");
-        Object result = null;
+        javaScriptEngine.put(ScriptEngine.FILENAME, "solution.js");
+        Object result;
         try {
             result = javaScriptEngine.eval(script);
         } catch (ScriptException e) {
-            result = e.getMessage();
+            result = prepareErrorMessage(e);
         }
         return result != null ? result : "No Result";
+    }
+
+    private String prepareErrorMessage(ScriptException e) {
+        String message = e.getMessage();
+        String separator = ": ";
+        int index = message.lastIndexOf(separator) + separator.length();
+        return message.substring(index, message.length());
     }
 
 }
