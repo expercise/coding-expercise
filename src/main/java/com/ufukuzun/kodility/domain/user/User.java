4404431bf273ea506c2822e34f4461d68466aaed
Ufuk Uzun <ufukuzun.ce@gmail.com>
2014-03-31 23:54:43 +0300

    insert sample challenge if no challenge exists

--- src/main/java/com/ufukuzun/kodility/domain/user/User.java
+++ src/main/java/com/ufukuzun/kodility/domain/user/User.java
@@ -1,7 +1,38 @@
 package com.ufukuzun.kodility.domain.user;
 
+import org.springframework.data.annotation.Id;
+
 public class User {
 
-    // TODO ufuk: complete after Spring Security and Hibernate configuration
+    @Id
+    private String id;
+
+    private String email;
+
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
