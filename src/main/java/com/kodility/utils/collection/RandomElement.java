5a372b315fc5380d2bed091cc3c77d1df778d81e
Ufuk Uzun <ufukuzun.ce@gmail.com>
2014-12-14 23:06:27 +0200

    tests are fixed for new non-nullable field of challenge (outputType)

--- src/main/java/com/kodility/utils/collection/RandomElement.java
+++ src/main/java/com/kodility/utils/collection/RandomElement.java
@@ -3,7 +3,9 @@ package com.kodility.utils.collection;
 import org.apache.commons.lang3.RandomUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public final class RandomElement {
 
@@ -11,8 +13,12 @@ public final class RandomElement {
     }
 
     public static <T> T from(Collection<T> collection) {
-        ArrayList<T> list = new ArrayList<>(collection);
+        List<T> list = new ArrayList<>(collection);
         return list.isEmpty() ? null : list.get(RandomUtils.nextInt(0, list.size()));
+    }
+
+    public static <T> T from(T[] array) {
+        return from(Arrays.asList(array));
     }
 
 }
--- src/test/java/com/kodility/testutils/builder/ChallengeBuilder.java
+++ src/test/java/com/kodility/testutils/builder/ChallengeBuilder.java
@@ -3,6 +3,8 @@ package com.kodility.testutils.builder;
 import com.kodility.domain.challenge.Challenge;
 import com.kodility.domain.level.Level;
 import com.kodility.domain.user.User;
+import com.kodility.enums.DataType;
+import com.kodility.utils.collection.RandomElement;
 
 public class ChallengeBuilder extends AbstractEntityBuilder<Challenge, ChallengeBuilder> {
 
@@ -14,13 +16,17 @@ public class ChallengeBuilder extends AbstractEntityBuilder<Challenge, Challenge
 
     private Level level;
 
+    private DataType outputType = RandomElement.from(DataType.values());
+
     @Override
     protected Challenge doBuild() {
+
         Challenge challenge = new Challenge();
         challenge.setApproved(approved);
         challenge.setUser(user);
         challenge.setPoint(point);
         challenge.setLevel(level);
+        challenge.setOutputType(outputType);
         if (level != null) {
             level.getChallenges().add(challenge);
         }
@@ -44,6 +50,11 @@ public class ChallengeBuilder extends AbstractEntityBuilder<Challenge, Challenge
 
     public ChallengeBuilder level(Level level) {
         this.level = level;
+        return this;
+    }
+
+    public ChallengeBuilder outputType(DataType outputType) {
+        this.outputType = outputType;
         return this;
     }
 
