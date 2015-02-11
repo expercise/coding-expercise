d4b6e7aaf13923779c2a089ea3e1de89a2b97106
Bay Batu <batuhanbayrakci@gmail.com>
2015-02-11 22:53:19 +0200

    we can use configuration sql until liquibase.

---
+++ src/main/resources/sqls/configurations.sql
@@ -0,0 +1,2 @@
+-- baybatu - 11/02/2015 22:50
+INSERT INTO Configuration (name,value) VALUES ('google.analytics.script',"(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-59509615-1', 'auto'); ga('require', 'displayfeatures'); ga('require', 'linkid', 'linkid.js'); ga('send', 'pageview');");
