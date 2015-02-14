65c4f3775957b3f3a4e24af2c41fc7da7eb27d29
Ufuk Uzun <ufukuzun.ce@gmail.com>
2015-02-14 16:36:14 +0200

    new *.properties structure

--- src/main/java/com/expercise/configuration/PropertiesConfiguration.java
+++ src/main/java/com/expercise/configuration/PropertiesConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource(ignoreResourceNotFound = true, value = {"classpath:${targetEnvironment:development}.properties", "classpath:developerSpecific.properties", "classpath:build.properties"})
+@PropertySource(ignoreResourceNotFound = true, value = {"classpath:default.properties", "classpath:environmentSpecific.properties", "classpath:build.properties"})
 public class PropertiesConfiguration {
 
     @Bean
