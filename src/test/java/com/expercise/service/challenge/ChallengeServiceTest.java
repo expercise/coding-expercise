c16f86d2dbe8da5c9dbccee0549a0a50caefef21
Bay Batu <batuhanbayrakci@gmail.com>
2015-04-12 18:35:05 +0300

    new Tests for user state cache methods

--- src/test/java/com/expercise/service/challenge/ChallengeServiceTest.java
+++ src/test/java/com/expercise/service/challenge/ChallengeServiceTest.java
@@ -1,11 +1,19 @@
 package com.expercise.service.challenge;
 
 import com.expercise.domain.challenge.Challenge;
+import com.expercise.domain.challenge.TestCase;
+import com.expercise.domain.challenge.TestCaseInputValue;
 import com.expercise.domain.user.User;
 import com.expercise.enums.ProgrammingLanguage;
+import com.expercise.interpreter.TestCaseModel;
+import com.expercise.interpreter.TestCaseWithResult;
+import com.expercise.interpreter.TestCasesWithSourceCacheModel;
+import com.expercise.interpreter.TestCasesWithSourceModel;
 import com.expercise.service.language.SignatureGeneratorService;
 import com.expercise.service.user.AuthenticationService;
 import com.expercise.testutils.builder.ChallengeBuilder;
+import com.expercise.testutils.builder.TestCaseBuilder;
+import com.expercise.testutils.builder.TestCaseInputValueBuilder;
 import com.expercise.testutils.builder.UserBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +21,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,6 +41,9 @@ public class ChallengeServiceTest {
 
     @Mock
     private AuthenticationService authenticationService;
+
+    @Mock
+    private UserTestCaseStateService userTestCaseStateService;
 
     @Test
     public void shouldPrepareChallengeSignatures() {
@@ -49,5 +63,58 @@ public class ChallengeServiceTest {
         assertThat(resultMap.get(ProgrammingLanguage.JavaScript.getShortName()), equalTo("function solution() {}"));
         assertThat(resultMap.get(ProgrammingLanguage.Java.getShortName()), equalTo("public class Solution { public Integer solution() {} }"));
     }
+
+    @Test
+    public void shouldGetUserStateFromCache() {
+        Challenge challenge = new ChallengeBuilder().id(1L).build();
+
+        TestCaseInputValue inputValue1 = new TestCaseInputValueBuilder().id(1L).inputValue("i1").build();
+        TestCaseInputValue inputValue2 = new TestCaseInputValueBuilder().id(2L).inputValue("i2").build();
+        TestCase testCase = new TestCaseBuilder().id(1L).challenge(challenge).inputs(inputValue1, inputValue2).output("o1").build();
+
+        challenge.addTestCase(testCase);
+
+        List<TestCaseWithResult> testCasesWithResults = new ArrayList<>();
+        testCasesWithResults.add(new TestCaseWithResult(testCase));
+        TestCasesWithSourceCacheModel cacheModel = new TestCasesWithSourceCacheModel("sourceCode", testCasesWithResults);
+
+        when(userTestCaseStateService.getUserTestCasesOf(challenge, ProgrammingLanguage.Java)).thenReturn(cacheModel);
+
+        TestCasesWithSourceModel userState = service.getUserStateFor(challenge, ProgrammingLanguage.Java);
+
+        assertThat(userState.getTestCaseModels().size(), equalTo(1));
+        TestCaseModel model = userState.getTestCaseModels().get(0);
+        assertThat(model.getInputs().get(0), equalTo("i1"));
+        assertThat(model.getInputs().get(1), equalTo("i2"));
+        assertThat(model.getOutput(), equalTo("o1"));
+    }
+
+    @Test
+    public void shouldGetUserNewStateFromCacheAfterReset() {
+        Challenge challenge = new ChallengeBuilder().id(1L).build();
+
+        TestCaseInputValue inputValue1 = new TestCaseInputValueBuilder().id(1L).inputValue("i1").build();
+        TestCaseInputValue inputValue2 = new TestCaseInputValueBuilder().id(2L).inputValue("i2").build();
+        TestCase testCase = new TestCaseBuilder().id(1L).challenge(challenge).inputs(inputValue1, inputValue2).output("o1").build();
+
+        challenge.addTestCase(testCase);
+
+        List<TestCaseWithResult> testCasesWithResults = new ArrayList<>();
+        testCasesWithResults.add(new TestCaseWithResult(testCase));
+        TestCasesWithSourceCacheModel cacheModel = new TestCasesWithSourceCacheModel("sourceCode", testCasesWithResults);
+
+        when(userTestCaseStateService.getUserTestCasesOf(challenge, ProgrammingLanguage.Java)).thenReturn(cacheModel);
+
+        TestCasesWithSourceModel userState = service.resetUserStateFor(challenge, ProgrammingLanguage.Java);
+
+        verify(userTestCaseStateService).resetUserState(challenge, ProgrammingLanguage.Java);
+
+        assertThat(userState.getTestCaseModels().size(), equalTo(1));
+        TestCaseModel model = userState.getTestCaseModels().get(0);
+        assertThat(model.getInputs().get(0), equalTo("i1"));
+        assertThat(model.getInputs().get(1), equalTo("i2"));
+        assertThat(model.getOutput(), equalTo("o1"));
+    }
+
 
 }
