5ff5b91ad7a4e418f74a61a6c4c48166d313ea81
Bay Batu <batuhanbayrakci@gmail.com>
2016-02-05 18:39:34 +0200

    prefer not to use double curly init

--- src/main/java/com/expercise/dao/user/UserConnectionDao.java
+++ src/main/java/com/expercise/dao/user/UserConnectionDao.java
@@ -5,6 +5,7 @@ import com.expercise.domain.user.UserConnection;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class UserConnectionDao extends AbstractHibernateDao<UserConnection> {
@@ -14,10 +15,10 @@ public class UserConnectionDao extends AbstractHibernateDao<UserConnection> {
     }
 
     public UserConnection findBy(String providerId, String providerUserId) {
-        return findOneBy(new HashMap<String, Object>() {{
-            put("providerId", providerId);
-            put("providerUserId", providerUserId);
-        }});
+        Map<String, Object> params = new HashMap<>();
+        params.put("providerId", providerId);
+        params.put("providerUserId", providerUserId);
+        return findOneBy(params);
     }
 
 }
