b65f51d7989b4d009e8d993033bc753c2e433e50
Bay Batu <batuhanbayrakci@gmail.com>
2016-02-05 18:39:58 +0200

    missing serializable uid

--- src/main/java/com/expercise/exception/ExperciseGenericException.java
+++ src/main/java/com/expercise/exception/ExperciseGenericException.java
@@ -2,6 +2,8 @@ package com.expercise.exception;
 
 public class ExperciseGenericException extends RuntimeException {
 
+    private static final long serialVersionUID = -4059537275690891978L;
+
     public ExperciseGenericException(String message, Throwable exception) {
         super(message, exception);
     }
