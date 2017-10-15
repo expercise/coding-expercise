61051a0c67afaec6fcb346bfabba5113d4624c9b
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-10-15 19:03:28 +0300

    spring session redis support

---
+++ src/main/java/com/expercise/configuration/SpringSessionRedisConfiguration.java
@@ -0,0 +1,7 @@
+package com.expercise.configuration;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class SpringSessionRedisConfiguration {
+}
