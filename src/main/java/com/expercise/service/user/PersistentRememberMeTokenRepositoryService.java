b69ac666652eb38c4ac58d2adc32d238ac779da3
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-10-15 18:49:11 +0300

    fix remember me transaction issue

--- src/main/java/com/expercise/service/user/PersistentRememberMeTokenRepositoryService.java
+++ src/main/java/com/expercise/service/user/PersistentRememberMeTokenRepositoryService.java
@@ -4,6 +4,7 @@ import com.expercise.domain.user.RememberMeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -12,16 +13,19 @@ public class PersistentRememberMeTokenRepositoryService implements PersistentTok
     @Autowired
     private UserService userService;
 
+    @Transactional
     @Override
     public void createNewToken(PersistentRememberMeToken token) {
         userService.saveRememberMeToken(token.getUsername(), token.getTokenValue(), token.getSeries(), token.getDate());
     }
 
+    @Transactional
     @Override
     public void updateToken(String series, String tokenValue, Date lastUsed) {
         userService.updateRememberMeToken(tokenValue, series, lastUsed);
     }
 
+    @Transactional
     @Override
     public PersistentRememberMeToken getTokenForSeries(String seriesId) {
         RememberMeToken rememberMeToken = userService.findRememberMeToken(seriesId);
@@ -31,6 +35,7 @@ public class PersistentRememberMeTokenRepositoryService implements PersistentTok
         return new PersistentRememberMeToken(rememberMeToken.getEmail(), seriesId, rememberMeToken.getToken(), rememberMeToken.getLastUsedTime());
     }
 
+    @Transactional
     @Override
     public void removeUserTokens(String userId) {
         userService.removeRememberMeToken(userId);
