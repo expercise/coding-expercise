d256ec31d3f7843f60e85a3c1af91e3ff2e98494
Bay Batu <batuhanbayrakci@gmail.com>
2018-02-11 18:48:22 +0300

    stop containers before start containers

--- devsetup/start.sh
+++ devsetup/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+### STOP CONTAINERS
+
+echo "PostgreSQL container is being stopped..."
+docker rm -f codingexpercise-postgres || true
+
+echo "Redis container is being stopped..."
+docker rm -f codingexpercise-redis || true
+
+### START CONTAINERS
+
 echo "PostgreSQL container is being started..."
 docker run --name codingexpercise-postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=123qwe -e POSTGRES_USER=root -e POSTGRES_DB=codingexpercise postgres
 
